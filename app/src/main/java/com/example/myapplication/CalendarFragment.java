aaef49d646a4bca8113dabe7c3153e1321ce2643
Ade11eda <adelb1nch@gmail.com>
2025-04-07 08:56:24 -0400

    Made some basic features for the calendar

--- app/src/main/java/com/example/myapplication/CalendarFragment.java
+++ app/src/main/java/com/example/myapplication/CalendarFragment.java
@@ -1,12 +1,22 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +33,18 @@ public class CalendarFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private HashMap<String, String[]> mealPlan = new HashMap<String, String[]>() {{
+        put("Monday", new String[]{"meal 1", "meal 2", "meal 3"});
+        put("Tuesday", new String[]{"Meal 4", "Meal 5", "Meal 6"});
+        put("Wednesday", new String[]{"meal 7", "meal 8", "meal 9"});
+        put("Thursday", new String[]{"meal 12", "meal 11", "meal 10"});
+        put("Friday", new String[]{"meal 13", "meal 14", "meal 15"});
+        put("Saturday", new String[]{"meal 18", "meal 17", "meal 16"});
+        put("Sunday", new String[]{"meal 19", "meal 20", "meal 21"});
+    }};
+
+    LinearLayout daysContainer;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -59,6 +81,39 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        daysContainer = view.findViewById(R.id.daysContainer);
+        View popupView = getLayoutInflater().inflate(R.layout.meal_plan, null);
+        TextView titleTextView = popupView.findViewById(R.id.popupTitle);
+        TextView mealsTextView = popupView.findViewById(R.id.popupMeals);
+        Button closeButton = popupView.findViewById(R.id.closePopupButton);
+        for (int i = 0; i <days.length; i++){
+            Button dayButton = new Button(getContext());
+            dayButton.setText(days[i]);
+            dayButton.setAllCaps(false);
+            dayButton.setPadding(24, 16, 24, 16);
+            String[] meals = mealPlan.get(days[i]);
+            dayButton.setOnClickListener(v -> {
+            String message = "Breakfast: " + meals[0] + "\n" + "Lunch: " + meals[1] + "\n" + "Dinner: " + meals[2];
+            mealsTextView.setText(message);
+
+            PopupWindow popupWindow = new PopupWindow(
+                    popupView,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            popupWindow.setFocusable(true);
+            popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            popupWindow.setOutsideTouchable(true);
+
+            popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+            closeButton.setOnClickListener(view1 -> popupWindow.dismiss());
+        });
+
+            daysContainer.addView(dayButton);
+        }
+        return view;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.myapplication.CalendarFragment;
+import com.example.myapplication.IngredientsFragment;
+import com.example.myapplication.RecipeFragment;
+
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
@@ -18,19 +19,41 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+        replaceFragment(new CalendarFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.calendar) {
+                replaceFragment(new CalendarFragment());
+            } else if (id == R.id.ingredients) {
+                replaceFragment(new IngredientsFragment());
+            } else if (id == R.id.recipes) {
+                replaceFragment(new RecipeFragment());
+            }
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); // Corrected
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
 }
