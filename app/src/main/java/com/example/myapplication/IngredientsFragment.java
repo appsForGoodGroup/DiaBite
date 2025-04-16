8849ab13b8d4a9edcf6d182b56fd66b033ef64fc
issaroha08 <152733162+issaroha08@users.noreply.github.com>
2025-04-16 10:32:17 -0400

    added to ingredientsFragment.java and fragment_ingredients.xml, list for the ingredients

--- app/src/main/java/com/example/myapplication/IngredientsFragment.java
+++ app/src/main/java/com/example/myapplication/IngredientsFragment.java
@@ -1,12 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +16,13 @@ import android.view.ViewGroup;
  */
 public class IngredientsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +30,6 @@ public class IngredientsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IngredientsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static IngredientsFragment newInstance(String param1, String param2) {
         IngredientsFragment fragment = new IngredientsFragment();
         Bundle args = new Bundle();
@@ -60,5 +53,24 @@ public class IngredientsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ingredients, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = view.findViewById(R.id.listview);
+        items = new ArrayList<>();
+
+        // Add your sample items
+        items.add("Apple");
+        items.add("Banana");
+        items.add("Orange");
+        items.add("Strawberry");
+        items.add("Kiwi");
+        items.add("Mango");
+
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
     }
 }
